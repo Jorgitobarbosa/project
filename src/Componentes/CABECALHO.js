acbcea25e73cf8ad1fdf12f582491ccd23645ed5
barbosa <jorgitobarbosa19@gmail.com>
2025-07-25 00:01:00 -0300

    Alteracao feita

---
+++ src/Componentes/CABECALHO.js
@@ -0,0 +1,13 @@
+import BannerCabecalho from "./Banner-Cabecalho";
+import ColunaCabecalho from "./Colunas-Cabecalho";
+
+function Cabecalho() {
+  return (
+    <div>
+      <BannerCabecalho />
+      <ColunaCabecalho />
+    </div>
+  );
+}
+
+export default Cabecalho;
